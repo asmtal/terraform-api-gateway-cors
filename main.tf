760c0f87c7bf3bd39719da42b156c024def58f7f
meza <meza@meza.hu>
2018-07-01 17:27:12 +0100

    no ,

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   http_method   = "${aws_api_gateway_method.options_method.http_method}"
   status_code   = "${aws_api_gateway_method_response.options_200.status_code}"
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
     "method.response.header.Access-Control-Allow-Methods" = "'${var.methods}'",
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
     "method.response.header.Access-Control-Allow-Credentials" = "'false'"
